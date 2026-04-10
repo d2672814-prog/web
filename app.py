e449d179b4b942863ec4a6685d96078f71f338e2
d2672814-prog <d2672814@gmail.com>
2026-04-10 11:03:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,21 @@ np.random.seed(42)
 def train_model():
     n = 15000 
     
+    # Параметры
     solar_flux = np.random.normal(150, 50, n)
     kp_index = np.random.uniform(0, 9, n)
     wind_speed = np.random.normal(400, 150, n)
     bz = np.random.uniform(-20, 10, n)
     density = np.random.normal(5, 10, n)
 
-    score = (kp_index * 20) + (wind_speed * 0.05) - (bz * 16) + (density * 4)
+    score = (kp_index * 20) + (wind_speed * 0.05) - (bz * 15) + (density * 2)
+    
     score += np.random.normal(0, 15, n)
 
-    storm = (score > 170).astype(int) 
+    storm = (score > 200).astype(int)
 
     X = np.column_stack([solar_flux, kp_index, wind_speed, bz, density])
-    
+   
     model = RandomForestClassifier(
         n_estimators=150, 
         max_depth=5, 
